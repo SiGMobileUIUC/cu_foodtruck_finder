e348b05784f61a3a2bf98b7d59333c40f4fa561e
Justin Ouyang <jstnoy@gmail.com>
2024-11-05 16:31:25 -0600

    Polished Login Page

--- lib/src/app/pages/login.dart
+++ lib/src/app/pages/login.dart
@@ -16,18 +16,44 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const TextField(
-              decoration: InputDecoration(labelText: "Username"),
+              decoration: InputDecoration(
+                labelText: "Username",
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
             ),
             const SizedBox(height: 20),
             const TextField(
-              decoration: InputDecoration(labelText: "Password"),
+              decoration: InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
               obscureText: true,
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text("Login"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Align(
+                   alignment: Alignment.centerLeft,
+                    child: ElevatedButton(
+                     onPressed: () {},
+                     child: const Text("Forgot Password?"),
+                    ),
+                 ),
+                ),
+                Expanded(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Login"),
+                  ),
+                ),
+               ),
+              ],
             ),
           ],
         ),
